14280ebeb0f60cc5f68f5f7fa9d74a1c22ddded1
Uvarov1tte <ngqchia@gmail.com>
2026-03-09 16:33:06 +0700

    add font (einer grotesk)

---
+++ app/_utils/fonts/fonts.tsx
@@ -0,0 +1,45 @@
+import localFont from "next/font/local";
+export const einerGrotesk = localFont({
+  src: [
+    {
+      path: "./einer_grotesk/EinerGrotesk-Hairline.woff2",
+      weight: "50",
+      style: "normal",
+    },
+    {
+      path: "./einer_grotesk/EinerGrotesk-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "./einer_grotesk/EinerGrotesk-ExtraLight.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./einer_grotesk/EinerGrotesk-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./einer_grotesk/EinerGrotesk-Book.woff2",
+      weight: "350",
+      style: "normal",
+    },
+    {
+      path: "./einer_grotesk/EinerGrotesk-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./einer_grotesk/EinerGrotesk-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./einer_grotesk/EinerGrotesk-Heavy.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { einerGrotesk } from "./_utils/fonts/fonts";
+
 export default function Home() {
   return (
-    <div className="container">
-      <h1 className="mt-6">HOME</h1>
+    <div className="container grid grid-cols-12 grid-rows-16 gap-x-[32px]">
+      <h1 className={`title1 text-8xl font-black ${einerGrotesk.className}`}>hi, i'm</h1>
+      <h1 className={`title2 italic text-8xl font-black italic ${einerGrotesk.className}`}>alm4nditte</h1>
     </div>
   );
 }
